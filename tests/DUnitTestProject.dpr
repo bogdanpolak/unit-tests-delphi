1bd5410376018b32d05dd25d634be1c9a9349d79
Bogdan Polak <bogdanpol@gmail.com>
2021-02-18 21:56:53 +0100

    Added XMLTestRunner

--- tests/DUnitTestProject.dpr
+++ tests/DUnitTestProject.dpr
@@ -11,11 +11,13 @@ uses
   TestCase01 in 'TestCase01.pas',
   DataRepository in '..\src\MyModule\DataRepository.pas',
   Logic.MyModule in '..\src\MyModule\Logic.MyModule.pas' {MyModule: TDataModule},
+  XMLTestRunner2 in 'XMLTestRunner2.pas',
   Helper.IntegerList in '..\src\Helpers\Helper.IntegerList.pas';
 
 {$R *.RES}
 
 begin
   DUnitTestRunner.RunRegisteredTests;
+  XMLTestRunner2.RunRegisteredTests.Free;
 end.
 
